aa12f6c259e291789d1ce7fbb0b99a6f2f022000
bouhriz <abderrahimbouhriz@gmail.com>
2023-12-13 00:31:43 +0100

    Add backend

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
-  products: Array<any> = [
-    { id: 1, name: 'Product 1', price: 29.99, description: 'Description for Product 1',checked:true },
-    { id: 2, name: 'Product 2', price: 39.99, description: 'Description for Product 2',checked:false  },
-    { id: 3, name: 'Product 3', price: 49.99, description: 'Description for Product 3',checked:true  }
-  ];
+export class ProductsComponent implements OnInit{
+  products: Array<any> = [];
 
+  constructor(private http:HttpClient){}
+  
+  ngOnInit(){
+    
+    this.http.get<Array<any>>("http://localhost:3000/products").subscribe({
+      next : data=> {
+        this.products = data
+    },
+      error : err =>{
+        console.log('ERROR')
+      }
+   })
+  }
 
   handleCheckProduct(product: any) {
-    product.checked=!product.checked;
-    }
-
+    this.http.patch<any>(`http://localhost:3000/products/${product.id}`, { checked: !product.checked }).subscribe({
+      next : updatedProduct => {
+         product.checked=!product.checked;
+        }
+      })
+  }
 }
